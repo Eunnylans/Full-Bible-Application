dbff47501ecaf6dd28536f0b28ffa2da648b95ce
Eunice Abieyuwa Igbinedion <eunice.igbinedion@yahoo.com>
2024-08-17 08:51:28 +0200

    Updating Different Versions

--- src/Components/API/Api.jsx
+++ src/Components/API/Api.jsx
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 const API_URL = 'https://bible-api.com/';
 
+
 export const getVerse = async (verse) => {
     try {
         const response = await axios.get(`${API_URL}${verse}`);
@@ -12,3 +13,37 @@ export const getVerse = async (verse) => {
         throw error;
     }
 };
+
+
+export const getBooks = async () => {
+    try {
+        const response = await axios.get(`${API_URL}/books`);
+        return response.data;
+    } catch (error) {
+        console.error('Error fetching books:', error);
+        throw error;
+    }
+};
+
+
+export const getChapter = async (book, chapter) => {
+    try {
+        const response = await axios.get(`${API_URL}/${book}/${chapter}`);
+        return response.data;
+    } catch (error) {
+        console.error('Error fetching chapter:', error);
+        throw error;
+    }
+};
+
+
+export const getVerseByVersion = async (verse, version) => {
+    try {
+        const response = await axios.get(`${API_URL}/${verse}?translation=${version}`);
+        return response.data;
+    } catch (error) {
+        console.error('Error fetching verse:', error);
+        throw error;
+    }
+};
+
